1ceecf60347d1433b0c14c146031625864609c25
Doug Bleek <dougbleek@gmail.com>
2023-02-20 16:23:00 -0500

    "Adjusted django settings again"

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,9 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-from dotenv import load_dotenv
-load_dotenv()
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO')
+SECRET_KEY = 'django-insecure-$gwcms6*@%3ht#&vevy#2xffxyg88ngz(4ez%0dh(&z$)-cy7^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
